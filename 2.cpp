325c009e9a309addf953acf77ff70ce48254b2fd
Timdevcc <118447625+Timdevcc@users.noreply.github.com>
2026-04-05 21:23:32 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,26 +1,67 @@
 #include <iostream>
-#include <array>
-#include <cstdint>
-#include <stdexcept>
-#include <type_traits>
-#include <vector>
+#include <sstream>
+#include <string>
+#include <cmath>
 
-template <class C, class Pred> C myfilter (const C &c, Pred pred) {
-    typename C result {};
-    auto it = c.begin();
-    while (it != c.end()) {
-        if (pred(*it)) {
-            result.push_back(*it);
-        }
-        it++;
+class Figure {
+public:
+    virtual double get_square() const = 0;
+    virtual ~Figure();
+};
+
+class Rectangle : public Figure {
+private:
+    double w, h;
+
+public:
+    Rectangle(double w, double h) : w(w), h(h) {}
+
+    double get_square() const override {
+        return w * h;
     }
-    return result;
-}
 
-int main() {
-    
-    
-    
-}
-    
+    static Figure* make(const std::string& str) {
+        std::istringstream iss(str);
+        double w, h;
+        iss >> w >> h;
+        return new Rectangle(w, h);
+    }
+};
 
+class Square : public Figure {
+private:
+    double a;
+
+public:
+    Square(double a) : a(a) {}
+
+    double get_square() const override {
+        return a * a;
+    }
+
+    static Figure* make(const std::string& str) {
+        std::istringstream iss(str);
+        double a;
+        iss >> a;
+        return new Square(a);
+    }
+};
+
+class Circle : public Figure {
+private:
+    double r;
+
+public:
+    Circle(double r) : r(r) {}
+
+    double get_square() const override {
+        return M_PI * r * r;
+    }
+
+    static Figure* make(const std::string& str) {
+        std::istringstream iss(str);
+        double r;
+        iss >> r;
+        return new Circle(r);
+    }
+};
